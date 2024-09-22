497f862641fe1cdb3c14d5b5d9204ce16d5bdecb
erick.fraga <erick.fraga@globalsys.com.br>
2024-09-22 01:28:26 -0300

    v0.1

--- src/components/main-side-form.tsx
+++ src/components/main-side-form.tsx
@@ -111,7 +111,7 @@ export const MainSideForm = () => {
 		setCursorPosition(e.target.selectionStart); // Captura a posição do cursor
 	};
 
-	// Função para simular o cursor no texto
+	// Função para simular o cursor no texto asda
 	const getContentWithCursor = () => {
 		const beforeCursor = content.slice(0, cursorPosition);
 		const afterCursor = content.slice(cursorPosition);
