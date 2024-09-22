a16adc4d31ac44b3b9dd0efbc8e17e7b7fd691bb
erick.fraga <erick.fraga@globalsys.com.br>
2024-09-22 11:40:51 -0300

    fix: containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useRef } from "react";
 import "./App.css";
+import { Toaster } from "./components/ui/toaster";
+import { TooltipProvider } from "./components/ui/tooltip";
 import { backgroundJson, foregroundForeground } from "./consts/scrolling-text";
 import MainRouter from "./pages";
 
@@ -10,43 +12,46 @@ function App() {
 	const foregroundJsonRef = useRef<HTMLPreElement>(null);
 
 	return (
-		<div
-			className="flex w-screen h-screen bg-cover bg-center bg-main  overflow-hidden block"
-			// style={{
-			// 	backgroundImage: `url(${background})`,
-			// }}
-		>
-			<div className="bg-main-gradient backdrop-blur-[2px] w-full h-full bg-opacity-50  flex flex-row">
-				<div className="absolute blur-[6px] -top-[50px] -left-[40%] origin-top-left -rotate-[20deg]  text-[#BFFF00]/20 text-[80px] font-normal font-['JetBrains Mono']">
-					<pre
-						id="background-json"
-						ref={backgroundJsonRef}
-						className="animate-scroll-reverse"
-						style={{
-							animationDuration: `${(backgroundJsonRef?.current?.offsetHeight ?? 1) / 100}s`,
-						}}
-						// style={{
-						// 	transform: `translateY(1.95%)`,
-						// }}
-					>
-						{JSON.stringify(foregroundForeground, null, 4)}
-					</pre>
+		<TooltipProvider>
+			<Toaster />
+			<div
+				className="flex w-screen h-screen bg-cover bg-center bg-main  overflow-hidden block"
+				// style={{
+				// 	backgroundImage: `url(${background})`,
+				// }}
+			>
+				<div className="bg-main-gradient backdrop-blur-[2px] w-full h-full bg-opacity-50  flex flex-row">
+					<div className="absolute blur-[6px] -top-[50px] -left-[40%] origin-top-left -rotate-[20deg]  text-[#BFFF00]/20 text-[80px] font-normal font-['JetBrains Mono']">
+						<pre
+							id="background-json"
+							ref={backgroundJsonRef}
+							className="animate-scroll-reverse"
+							style={{
+								animationDuration: `${(backgroundJsonRef?.current?.offsetHeight ?? 1) / 100}s`,
+							}}
+							// style={{
+							// 	transform: `translateY(1.95%)`,
+							// }}
+						>
+							{JSON.stringify(foregroundForeground, null, 4)}
+						</pre>
+					</div>
+					<div className="absolute blur-[4px] -top-[7px] left-[50%] origin-top-left rotate-[30deg] text-[#BFFF00]/30 text-5xl font-normal font-['JetBrains Mono']">
+						<pre
+							id="foreground-json"
+							ref={foregroundJsonRef}
+							className="animate-scroll"
+							style={{
+								animationDuration: `${(foregroundJsonRef?.current?.offsetHeight ?? 1) / 120}s`,
+							}}
+						>
+							{JSON.stringify(backgroundJson, null, 4)}
+						</pre>
+					</div>
+					<MainRouter />
 				</div>
-				<div className="absolute blur-[4px] -top-[7px] left-[50%] origin-top-left rotate-[30deg] text-[#BFFF00]/30 text-5xl font-normal font-['JetBrains Mono']">
-					<pre
-						id="foreground-json"
-						ref={foregroundJsonRef}
-						className="animate-scroll"
-						style={{
-							animationDuration: `${(foregroundJsonRef?.current?.offsetHeight ?? 1) / 120}s`,
-						}}
-					>
-						{JSON.stringify(backgroundJson, null, 4)}
-					</pre>
-				</div>
-				<MainRouter />
 			</div>
-		</div>
+		</TooltipProvider>
 	);
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { StuffServiceProvider } from "./hooks/stuff-service.tsx";
 import "./index.css";
-import { Toaster } from "./components/ui/toaster.tsx";
-import { TooltipProvider } from "./components/ui/tooltip.tsx";
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<TooltipProvider>
-			<StuffServiceProvider>
-				<App />
-				<Toaster />
-			</StuffServiceProvider>
-		</TooltipProvider>
+		<StuffServiceProvider>
+			<App />
+		</StuffServiceProvider>
 	</StrictMode>,
 );
