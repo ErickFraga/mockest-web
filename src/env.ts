eceef9809187fa5555c0c824795c77bfe6a78ada
erick.fraga <erick.fraga@globalsys.com.br>
2024-10-01 23:24:51 -0300

    fix: env

--- src/env.ts
+++ src/env.ts
@@ -2,19 +2,12 @@ import { z } from "zod";
 
 // Define your environment variables schema
 const envSchema = z.object({
-	VITE_ENV_MODE: z.enum(["dev", "prod"]),
 	VITE_API_URL: z.string().url(),
 	VITE_API_KEY: z.string().default(""),
 });
 
 // Validate environment variables using the schema
-const parsedEnv = envSchema.safeParse({
-	...import.meta.env,
-	VITE_API_URL:
-		import.meta.env.VITE_DEV_MODE === "dev"
-			? "http://localhost:3000"
-			: import.meta.env.VITE_API_URL,
-});
+const parsedEnv = envSchema.safeParse(import.meta.env);
 
 // Handle validation resul
 if (!parsedEnv.success) {
